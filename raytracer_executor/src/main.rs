d6c903ac6f0a86238f2b957ba315887dc45519b7
Michael <v.mi@gmx.de>
2020-06-21 13:02:11 +0200

    split raytracer into crates

--- raytracer_executor/src/main.rs
+++ raytracer_executor/src/main.rs
@@ -1,37 +1,7 @@
 use raytracer_shared::*;
 use raytracer_plugin_line::InfiniteLine;
+use raytracer_plugin_circle::Circle;
 
-pub struct Circle {
-    center: Vec2,
-    radius_squared: f32,
-}
-
-impl Circle {
-    pub fn new(center: Vec2, radius: f32) -> Self {
-        Self {
-            center,
-            radius_squared: radius.powi(2),
-        }
-    }
-}
-
-impl Intersectable for Circle {
-    fn intersect(&self, ray: &Ray) -> Option<Intersection> {
-        let o = ray.position() - self.center;
-        let projection = ray.direction() * o;
-        let discrimanant = self.radius_squared + projection.powi(2) - o * o;
-        if discrimanant <= 0. {
-            None
-        } else {
-            let root = discrimanant.sqrt();
-            let first = -projection - root;
-            let distance = if first < 0. { first + root * 2. } else { first };
-            let hit_point_relative_circle_center = o + ray.direction() * distance;
-            let normal = -1.0 * hit_point_relative_circle_center.normalize();
-            Some(Intersection { distance, normal })
-        }
-    }
-}
 
 fn main() {
     let y_axis = InfiniteLine::new(Vec2::new(0., 0.), Vec2::new(0., 1.));
