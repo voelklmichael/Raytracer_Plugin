263ce30477b1442cd3990005d5fa5c42d88a9829
Michael <v.mi@gmx.de>
2020-06-21 13:48:01 +0200

    versioning fiddling

--- raytracer_shared/src/lib.rs
+++ raytracer_shared/src/lib.rs
@@ -7,11 +7,10 @@ pub use intersection::{Intersectable, Intersection};
 mod scene;
 pub use scene::Scene;
 
-fn version() -> (u32, u32, u32) {
+pub fn version() -> (u32, u32, u32) {
     use pkg_version::{pkg_version_major, pkg_version_minor, pkg_version_patch};
     const MAJOR: u32 = pkg_version_major!();
     const MINOR: u32 = pkg_version_minor!();
     const PATCH: u32 = pkg_version_patch!();
     (MAJOR, MINOR, PATCH)
 }
-pub use version as line_version;
